f900573e768e6b3081912f07ba2722aab8a168c2
sudongxu <sdx@192.168.0.100>
2018-05-05 21:02:31 +0800

    修改部分bug 2.0

--- bak/xumi.sql
+++ bak/xumi.sql
@@ -5,7 +5,7 @@
  Source Server Type    : MySQL
  Source Server Version : 50721
  Source Host           : localhost
- Source Database       : fang
+ Source Database       : xumi
 
  Target Server Type    : MySQL
  Target Server Version : 50721
@@ -18,10 +18,10 @@ SET NAMES utf8;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
---  Table structure for `fang_admin`
+--  Table structure for `xm_admin`
 -- ----------------------------
-DROP TABLE IF EXISTS `fang_admin`;
-CREATE TABLE `fang_admin` (
+DROP TABLE IF EXISTS `xm_admin`;
+CREATE TABLE `xm_admin` (
   `admin_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_name` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
@@ -38,17 +38,17 @@ CREATE TABLE `fang_admin` (
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
---  Records of `fang_admin`
+--  Records of `xm_admin`
 -- ----------------------------
 BEGIN;
-INSERT INTO `fang_admin` VALUES ('1', 'root', '137504949@qq.com', '$2y$10$Lxf7aWAPtAruUTBS.7VBP.CRjobfsngn7gb3MoO7zJn8CjOt/6c2u', 'F8oYCzOiJgamM1VNW8GgJTQqbK9lHbSfhT8pqJIwDfmTfXhE3TnOXrZAv3rP', '2017-09-25 08:04:57', '2017-09-28 05:03:33', null, '0', '1', '0');
+INSERT INTO `xm_admin` VALUES ('1', 'root', '137504949@qq.com', '$2y$10$Lxf7aWAPtAruUTBS.7VBP.CRjobfsngn7gb3MoO7zJn8CjOt/6c2u', 'F8oYCzOiJgamM1VNW8GgJTQqbK9lHbSfhT8pqJIwDfmTfXhE3TnOXrZAv3rP', '2017-09-25 08:04:57', '2017-09-28 05:03:33', null, '0', '1', '0');
 COMMIT;
 
 -- ----------------------------
---  Table structure for `fang_admin_log`
+--  Table structure for `xm_admin_log`
 -- ----------------------------
-DROP TABLE IF EXISTS `fang_admin_log`;
-CREATE TABLE `fang_admin_log` (
+DROP TABLE IF EXISTS `xm_admin_log`;
+CREATE TABLE `xm_admin_log` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `content` varchar(200) NOT NULL COMMENT '内容',
   `created_at` timestamp NULL DEFAULT NULL COMMENT '添加时间',
@@ -60,17 +60,17 @@ CREATE TABLE `fang_admin_log` (
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
---  Records of `fang_admin_log`
+--  Records of `xm_admin_log`
 -- ----------------------------
 BEGIN;
-INSERT INTO `fang_admin_log` VALUES ('1', '管理员[root]登录', '2018-04-05 22:57:44', 'root', '1', '127.0.0.1', '2018-04-05 22:57:44'), ('2', '[root]创建菜单-站点设置', '2018-04-05 23:22:25', 'root', '1', '127.0.0.1', '2018-04-05 23:22:25'), ('3', '管理员[root]退出登录', '2018-04-05 23:51:33', 'root', '1', '127.0.0.1', '2018-04-05 23:51:33'), ('4', '管理员[root]登录', '2018-04-05 23:53:13', 'root', '1', '127.0.0.1', '2018-04-05 23:53:13');
+INSERT INTO `xm_admin_log` VALUES ('1', '管理员[root]登录', '2018-04-05 22:57:44', 'root', '1', '127.0.0.1', '2018-04-05 22:57:44'), ('2', '[root]创建菜单-站点设置', '2018-04-05 23:22:25', 'root', '1', '127.0.0.1', '2018-04-05 23:22:25'), ('3', '管理员[root]退出登录', '2018-04-05 23:51:33', 'root', '1', '127.0.0.1', '2018-04-05 23:51:33'), ('4', '管理员[root]登录', '2018-04-05 23:53:13', 'root', '1', '127.0.0.1', '2018-04-05 23:53:13');
 COMMIT;
 
 -- ----------------------------
---  Table structure for `fang_admin_menus`
+--  Table structure for `xm_admin_menus`
 -- ----------------------------
-DROP TABLE IF EXISTS `fang_admin_menus`;
-CREATE TABLE `fang_admin_menus` (
+DROP TABLE IF EXISTS `xm_admin_menus`;
+CREATE TABLE `xm_admin_menus` (
   `menu_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `menu_name` varchar(50) NOT NULL DEFAULT '',
   `menu_icon` varchar(20) DEFAULT '',
@@ -80,17 +80,17 @@ CREATE TABLE `fang_admin_menus` (
 ) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
---  Records of `fang_admin_menus`
+--  Records of `xm_admin_menus`
 -- ----------------------------
 BEGIN;
-INSERT INTO `fang_admin_menus` VALUES ('1', '系统管理', 'fa-bar-chart-o', '', '0'), ('2', '角色管理', null, 'admin/role', '1'), ('3', '用户管理', null, 'admin/user', '1'), ('4', '菜单管理', '', 'admin/menu', '1'), ('5', '管理员日志', null, 'admin/admin-log', '1'), ('26', '站点设置', null, 'admin/system', '1');
+INSERT INTO `xm_admin_menus` VALUES ('1', '系统管理', 'fa-bar-chart-o', '', '0'), ('2', '角色管理', null, 'admin/role', '1'), ('3', '用户管理', null, 'admin/user', '1'), ('4', '菜单管理', '', 'admin/menu', '1'), ('5', '管理员日志', null, 'admin/admin-log', '1'), ('26', '站点设置', null, 'admin/system', '1');
 COMMIT;
 
 -- ----------------------------
---  Table structure for `fang_gadmin`
+--  Table structure for `xm_gadmin`
 -- ----------------------------
-DROP TABLE IF EXISTS `fang_gadmin`;
-CREATE TABLE `fang_gadmin` (
+DROP TABLE IF EXISTS `xm_gadmin`;
+CREATE TABLE `xm_gadmin` (
   `gid` smallint(5) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
   `gname` varchar(50) DEFAULT NULL COMMENT '组名',
   `limits` text COMMENT '权限内容',
@@ -99,17 +99,17 @@ CREATE TABLE `fang_gadmin` (
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
---  Records of `fang_gadmin`
+--  Records of `xm_gadmin`
 -- ----------------------------
 BEGIN;
-INSERT INTO `fang_gadmin` VALUES ('1', '后台管理员', '1,2,3,4,5,17,18,19,20,23,25,21,22,24'), ('2', '供应商', '17,18,19'), ('3', '出入库管理者', '17,20,23');
+INSERT INTO `xm_gadmin` VALUES ('1', '后台管理员', '1,2,3,4,5,17,18,19,20,23,25,21,22,24'), ('2', '供应商', '17,18,19'), ('3', '出入库管理者', '17,20,23');
 COMMIT;
 
 -- ----------------------------
---  Table structure for `fang_system`
+--  Table structure for `xm_system`
 -- ----------------------------
-DROP TABLE IF EXISTS `fang_system`;
-CREATE TABLE `fang_system` (
+DROP TABLE IF EXISTS `xm_system`;
+CREATE TABLE `xm_system` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `icp` varchar(50) COLLATE utf8_bin DEFAULT NULL COMMENT '备案号',
   `tongji` text COLLATE utf8_bin COMMENT '第三方统计代码',
@@ -126,10 +126,10 @@ CREATE TABLE `fang_system` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
---  Records of `fang_system`
+--  Records of `xm_system`
 -- ----------------------------
 BEGIN;
-INSERT INTO `fang_system` VALUES ('1', '郑州-100344334', null, '郑州赢鸿', '郑州二手房_郑州二手房出售_郑州二手房网(郑州赢鸿网)', 0xe98391e5b79ee4ba8ce6898be688bf5fe98391e5b79ee4ba8ce6898be688bfe587bae594ae5fe98391e5b79ee4ba8ce6898be688bfe7bd9128e98391e5b79ee8b5a2e9b8bfe7bd9129, 'fang.net', '0371-60166618', '郑州市郑东新区地润路5-3号', '873366181@qq.com', null, '18037698368');
+INSERT INTO `xm_system` VALUES ('1', '郑州-100344334', null, '郑州赢鸿', '郑州二手房_郑州二手房出售_郑州二手房网(郑州赢鸿网)', 0xe98391e5b79ee4ba8ce6898be688bf5fe98391e5b79ee4ba8ce6898be688bfe587bae594ae5fe98391e5b79ee4ba8ce6898be688bfe7bd9128e98391e5b79ee8b5a2e9b8bfe7bd9129, 'xumi.net', '0371-60166618', '郑州市郑东新区地润路5-3号', '873366181@qq.com', null, '18037698368');
 COMMIT;
 
 -- ----------------------------
--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -76,11 +76,11 @@
         <div class="row border-bottom">
             <nav class="navbar navbar-static-top" role="navigation" style="margin-bottom: 0">
                 <div class="navbar-header"><a class="navbar-minimalize minimalize-styl-2 btn btn-info " href="#"><i class="fa fa-bars"></i> </a>
-                    <form role="search" class="navbar-form-custom" method="post" action="#">
-                        <div class="form-group">
-                            <input type="text" placeholder="请输入您需要查找的内容 …" class="form-control" name="top-search" id="top-search">
-                        </div>
-                    </form>
+                    {{--<form role="search" class="navbar-form-custom" method="post" action="#">--}}
+                        {{--<div class="form-group">--}}
+                            {{--<input type="text" placeholder="请输入您需要查找的内容 …" class="form-control" name="top-search" id="top-search">--}}
+                        {{--</div>--}}
+                    {{--</form>--}}
                 </div>
                 <ul class="nav navbar-top-links navbar-right">
                     {{--<li class="dropdown">--}}
